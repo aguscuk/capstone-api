5c9c4ecfd3e192d571f526ff6430d71ecae58b28
aguscuk <aguscuk@gmail.com>
2020-05-15 20:40:39 +0700

    update app.py filter

--- app - Copy.py
+++ app - Copy.py
@@ -10,11 +10,7 @@ def home():
     return '''<h1>Capstone API</h1>
 <p>aguscuk capstone project api</p>'''
 
-def dict_factory(cursor, row):
-    d = {}
-    for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
+
 
 @app.route('/api/v1/resources/suppliers/all', methods=['GET'])
 def list_suppliers():
@@ -108,57 +104,32 @@ def page_not_found(e):
     return "<h1>404</h1><p>The resource could not be found.</p>", 404
 
 
-@app.route('/api/v1/resources/filter', methods=['GET'])
+@app.route('/api/v1/resources/books', methods=['GET'])
 def api_filter():
     query_parameters = request.args
 
-    country = query_parameters.get('country')
+    id = query_parameters.get('id')
+    published = query_parameters.get('published')
+    author = query_parameters.get('author')
 
-    query = "SELECT O.Id OrderId, \
-							   O.OrderDate, \
-							   O.RequiredDate, \
-							   O.ShippedDate, \
-							   O.ShipRegion, \
-							   O.ShipCountry, \
-							   Od.UnitPrice, \
-							   Od.Quantity, \
-							   Od.Discount, \
-							   P.ProductName, \
-							   ROUND(Od.UnitPrice * Od.Quantity, 2) AS SubTotal, \
-							   (Od.UnitPrice * Od.Quantity * Od.Discount) AS DiscPrice, \
-							   (Od.UnitPrice * Od.Quantity) - (Od.UnitPrice * Od.Quantity * Od.Discount) AS Total, \
-							   Cu.CompanyName AccountName, \
-							   Cu.ContactName, \
-							   Cu.ContactTitle, \
-							   Cu.Address, \
-							   Cu.City, \
-							   Cu.Region, \
-							   Cu.PostalCode, \
-							   Cu.Country, \
-							   Cu.Phone, \
-							   Cu.Fax, \
-							   Ca.CategoryName, \
-							   Ca.Description \
-							   FROM [Order] O \
-							   LEFT JOIN \
-							   OrderDetail Od ON O.Id = Od.OrderId \
-							   LEFT JOIN \
-							   Product P ON Od.ProductId = P.Id \
-							   LEFT JOIN \
-							   Customer Cu ON O.CustomerId = Cu.Id \
-							   LEFT JOIN \
-							   Category Ca ON P.CategoryId = Ca.Id WHERE"
+    query = "SELECT * FROM books WHERE"
     to_filter = []
 
-    if country:
-        query += ' Country=? AND'
-        to_filter.append(country)
-    if not (country):
+    if id:
+        query += ' id=? AND'
+        to_filter.append(id)
+    if published:
+        query += ' published=? AND'
+        to_filter.append(published)
+    if author:
+        query += ' author=? AND'
+        to_filter.append(author)
+    if not (id or published or author):
         return page_not_found(404)
 
     query = query[:-4] + ';'
 
-    conn = sqlite3.connect("data_input/Northwind_small.sqlite")
+    conn = sqlite3.connect('books.db')
     conn.row_factory = dict_factory
     cur = conn.cursor()
 
